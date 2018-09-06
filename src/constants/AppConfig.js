120e443328fb1c773dddffb31f7e7cc81b090c80
Snehal Kale <snehal.kale@eternussolutions.com>
2018-09-06 12:12:43 +0530

    changed server url

--- src/constants/AppConfig.js
+++ src/constants/AppConfig.js
@@ -2,8 +2,8 @@
  * App Config File
  */
 const AppConfig = {
-  serverURL: "http://localhost:3010" //local URL
-  //serverURL: "http://192.168.101.21:3010"
+  // serverURL: "http://localhost:3010" //local URL
+  serverURL: "http://192.168.101.21:3010" //production url
 };
 
 export default AppConfig;
