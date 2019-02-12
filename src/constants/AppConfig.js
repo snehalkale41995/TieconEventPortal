a91272f5dc288c623cd60852722605e6bd557c4f
Snehal Kale <snehal.kale@eternussolutions.com>
2019-02-12 19:04:31 +0530

    changed server url

--- src/constants/AppConfig.js
+++ src/constants/AppConfig.js
@@ -3,7 +3,7 @@
  */
 const AppConfig = {
   // serverURL: "http://localhost:3010" //local URL
-  serverURL: "http://192.168.101.21:3010" //production url
+  serverURL: "https://tieconevent.azurewebsites.net" //production url
 };
 
 export default AppConfig;
